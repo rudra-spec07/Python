96c099b0cccb0c79e6d0aba8bc56f5dd195a39f1
rudrakashchoubey <rudrachoubey70@gmail.com>
2025-12-14 20:25:40 +0530

    print function behaviour

---
+++ escape.py
@@ -0,0 +1,6 @@
+print("""Hello Programmers 
+      I am Rudra """)#using triple quotes for multi line string 
+print("Hello Ides \n I am learning Python")#using escape sequence \n for new line
+print("Hello Ides \t I am learning Python")#using escape sequence \t for tab space
+print("Hello Ides \\ I am learning Python")#using escape sequence \\ for backslash
+print("\"Helo I am using\"double quotes" )
---
+++ print.py
@@ -0,0 +1,10 @@
+print("hello world","Rudra",6)
+#by default after comma space is added
+print("Hello World")
+print("Rudra")
+#by default after print cursor moves to next line
+#We can choose for these two print function behaviours by using sep and end parameters
+print("Hello World","Rudra",6,sep="")#we are putting no space between these three values
+print("Hello World","Rudra",6,sep="---")#we are putting --- between these three values
+print("Hello World",end=",")#we are putting , at the end instead of new line")
+print("Rudra")
