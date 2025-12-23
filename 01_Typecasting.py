f7275456a3c8b574d33bd202cf2748076bdaa5a8
rudrakashchoubey <rudrachoubey70@gmail.com>
2025-12-23 14:48:08 +0530

    modified index slicing by converting integer into string

--- 01_Typecasting.py
+++ 01_Typecasting.py
@@ -1,10 +1,13 @@
-a=4
-print(a, type(a))
+# a=4
+# print(a, type(a))
 
-b="3"
-print(b, type(b))
+# b="3"
+# print(b, type(b))
 
-#Here we are converting string to integer 
-print(int(b), type(int(b)))
-c=str(a)
-print(c+b, type(c+b))
+# #Here we are converting string to integer 
+# print(int(b), type(int(b)))
+# c=str(a)
+# print(c+b, type(c+b))
+
+c=int(input("Enter a number: "))
+print(str(c)[::-1])
